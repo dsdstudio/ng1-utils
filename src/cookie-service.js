392daef1bfd638393ed1bb2bcf989b985fc925a9
보롱킴 <dsdgun@gmail.com>
2016-09-01 22:54:50 +0900

    domain 의존성 끊음

--- src/cookie-service.js
+++ src/cookie-service.js
@@ -1,21 +1,21 @@
 angular.module('app').service('CookieService', CookieService);
 
-function CookieService(phConfig) {
-    function addCookie(k, v) {
-        const t0 = encodeURIComponent(k) + '=' + encodeURIComponent(v) + '; domain=' + phConfig.serverDomain + '; path=/;';
+function CookieService() {
+    function addCookie(name, value, domain) {
+        const t0 = encodeURIComponent(name) + '=' + encodeURIComponent(value) + '; domain=' + domain + '; path=/;';
         document.cookie = t0;
     }
-    function removeCookie(k, v) {
-        document.cookie = encodeURIComponent(k) + '=' + encodeURIComponent(v) + '; domain=' + phConfig.serverDomain + '; path=/; expires=' + new Date(0).toGMTString();
+    function removeCookie(name, value, domain) {
+        document.cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value) + '; domain=' + domain + '; path=/; expires=' + new Date(0).toGMTString();
     }
     function getCookie(k) {
         let cookieFields = document.cookie.split('; ');
-        let i = cookieFields.length;
-        let cookies = {};
-        while(i--) {
-            let fieldTokens = cookieFields[i].split('=');
-            cookies[fieldTokens[0]] = fieldTokens[1];
-        }
+        let cookies = document.cookie.split('; ').reduce((r, v) => {
+            let fieldTokens = v.split('=');
+            r[fieldTokens[0]] = fieldTokens[1];
+            return r;
+        }, {});
+
         return cookies[k];
     }
     return {
